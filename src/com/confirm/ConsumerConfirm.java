58e0c5c33ff890853f0dfd29a001b8385c3c3154
刘乾坤 <Universe_Liu@163.com>
2020-10-13 22:27:54 +0800

    确认机制-COnfirm-Return

---
+++ src/com/confirm/ConsumerConfirm.java
@@ -0,0 +1,10 @@
+package com.confirm;
+
+/**
+ * @author lqk
+ * @Date 2020/10/13
+ * @Description
+ */
+public class ConsumerConfirm {
+    
+}
---
+++ src/com/confirm/ProducerConfirm.java
@@ -0,0 +1,9 @@
+package com.confirm;
+
+/**
+ * @author lqk
+ * @Date 2020/10/13
+ * @Description
+ */
+public class ProducerConfirm {
+}
